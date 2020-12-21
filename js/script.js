092051c6d45a112983d7a9142067db06b3d1741a
Your Name <you@example.com>
2020-12-21 18:32:48 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const sideNav = document.querySelectorAll('.sidenav');
+      M.Sidenav.init(sideNav)
+
+const slider = document.querySelectorAll('.slider');
+    M.Slider.init(slider, {
+        indicators: false,
+        height: 500,
+        transition: 600,
+        interval: 3000
+      });
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
